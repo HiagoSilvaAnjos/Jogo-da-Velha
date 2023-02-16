9cbd31beb21763e53439016ec613509de25ad157
Hiago Silva <hiagosilvaanjos7@gmail.com>
2023-02-15 21:03:23 -0300

    update

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const elementsBoard = document.querySelectorAll(".home-stage");
-const stageContainer = document.querySelector(".stage-container");
-
-for (let i of elementsBoard) {
-    i.addEventListener("click", function(e) {
-        let stageElement = e.target.classList[1];
-        console.log(`Você clicou na ${stageElement}`);
-    })
-}
